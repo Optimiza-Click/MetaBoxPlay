6be0be2ac1335ea9bf72656c457445d01bf40f26
Luis david del barrio <ldelbarrio@optimizaclick.com>
2017-06-23 12:58:09 +0200

    first version

---
+++ meta_box_display.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: CF7getParams
+Plugin URI: http://optimizaclick.com
+Description: Get values to form
+Author: Departamento de Desarrollo Optimizaclick
+Author URI: http://#
+Version: 1.0
+Copyright: 2016 - 2017
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! class_exists( 'MetaBoxPlay' ) ) {
+  class MetaBoxPlay {
+      public function __construct() {
+        add_action( 'admin_init', array( $this, 'AddMetaBoxCustomPostType' ) );
+        add_filter( 'save_post', array( $this, 'SaveMetaBoxCustomPostType' ), 10, 2 );
+      }
+
+      public function AddMetaBoxCustomPostType() {
+          add_meta_box("display_options_custom_post", "DisplayOptions", array( $this,"MetaBoxesDisplayCustomPostType"), "post", "normal", "low");
+      }
+
+      public function MetaBoxesDisplayCustomPostType() {
+          global $post;
+
+           $all_meta_boxes = get_post_meta($post->ID, '', false);
+
+           foreach($all_meta_boxes as $meta_box => $meta_box_value) {  ?>
+              <p>
+              <label>Option <?= $meta_box ?></label><br />
+              <textarea cols="150" rows="2" name="<?= $meta_box ?>" class="width99"><?= $meta_box_value[0] ?></textarea>
+              </p>
+           <?php }
+      }
+
+      public function SaveMetaBoxCustomPostType() {
+            global $post;
+
+            $all_meta_boxes = get_post_meta($post->ID, '', false);
+
+            $disabled = array('store_id', '_edit_lock', '_vc_post_settings');
+
+
+           foreach($all_meta_boxes as $single_metabox_key => $single_metabox_value) {
+               if(!in_array($single_metabox_key,$disabled)){
+                  $array_metas_options[] = $single_metabox_key;
+               }
+
+           }
+              if ( $post ) {
+                  foreach($_POST as $key => $value) {
+                      if(in_array($key, $array_metas_options)) {
+                           update_post_meta($post->ID, $key, $value);
+                      }
+                  }
+              }
+        }
+    }
+  $GLOBALS['MetaBoxPlay'] = new MetaBoxPlay();
+}
